6e0aeaee634124215f2f77483d2578624672a9d0
Dianaalejandra1446 <dianaleja04@hotmail.com>
2024-03-30 21:46:31 -0500

    Last Security Details

--- src/main/java/com/campusland/crud_cliente/security/JWTValidationFilter.java
+++ src/main/java/com/campusland/crud_cliente/security/JWTValidationFilter.java
@@ -1,22 +1,71 @@
 package com.campusland.crud_cliente.security;
 
 import java.io.IOException;
+import java.util.Objects;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.filter.OncePerRequestFilter;
 
+import com.campusland.crud_cliente.services.JWTService;
+import com.campusland.crud_cliente.services.JWTUserDetailService;
+
+import io.jsonwebtoken.ExpiredJwtException;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
-public class JWTValidationFilter extends OncePerRequestFilter{
+/**
+ * Filtro para validar tokens JWT en las solicitudes entrantes.
+ */
+public class JWTValidationFilter extends OncePerRequestFilter {
 
+    private final JWTService jwtService;
+    private final JWTUserDetailService jwtUserDetailService;
+
+    private static final Logger log = LoggerFactory.getLogger(JWTValidationFilter.class);
+
+    public static final String AUTHORIZATION_HEADER = "Authorization";
+    public static final String AUTHORIZATION_HEADER_BEARER = "Bearer";
+
+    /**
+     * Constructor del filtro.
+     */
+    public JWTValidationFilter(JWTService jwtService, JWTUserDetailService jwtUserDetailService) {
+        this.jwtService = jwtService;
+        this.jwtUserDetailService = jwtUserDetailService;
+    }
+
+    /**
+     * Método principal para la lógica de filtrado.
+     */
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
             throws ServletException, IOException {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'doFilterInternal'");
+        
+        // Obtener el encabezado de autorización de la solicitud
+        final var requestTokenHeader = request.getHeader(AUTHORIZATION_HEADER);
+        String username = null;
+        String jwt = null;
+
+        // Verificar si el encabezado de autorización comienza con "Bearer" y obtener el token JWT
+        if (Objects.nonNull(requestTokenHeader) && requestTokenHeader.startsWith(AUTHORIZATION_HEADER_BEARER)) {
+            jwt = requestTokenHeader.substring(7); // Extraer el token JWT
+            try {
+                // Obtener el nombre de usuario del token JWT
+                username = jwtService.getUsernameFromToken(jwt);
+            } catch (IllegalArgumentException e) {
+                // Manejar errores al obtener el nombre de usuario del token JWT
+                log.error("Error al obtener el nombre de usuario del token JWT: {}", e.getMessage());
+            } catch (ExpiredJwtException e) {
+                // Manejar advertencias de token JWT caducado
+                log.warn("Token JWT expirado: {}", e.getMessage());
+            }
+        }
+
+        // Continuar con el filtro llamando al siguiente filtro en la cadena
+        filterChain.doFilter(request, response);
     }
-
-
 }
+
--- src/main/java/com/campusland/crud_cliente/services/JWTService.java
+++ src/main/java/com/campusland/crud_cliente/services/JWTService.java
@@ -1,8 +1,12 @@
 package com.campusland.crud_cliente.services;
 
 import java.nio.charset.StandardCharsets;
+import java.util.Collections;
 import java.util.Date;
+import java.util.Map;
 import java.util.function.Function;
+
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
@@ -38,6 +42,34 @@ public class JWTService {
     private Boolean isTokenExpired(String token){
         final var expirationDate = this.getExpirationDateFromToken(token);
         return expirationDate.before(new Date());
+    }
+
+
+    public String getUsernameFromToken(String token) {
+        return this.getClaimsFromToken(token, Claims::getSubject);
+    }
+
+    public String generateToken(UserDetails userDetails) {
+        final Map<String, Object> claims = Collections.singletonMap("ROLES", userDetails.getAuthorities().toString());
+        return this.getToken(claims, userDetails.getUsername());
+    }
+    private String getToken(Map<String, Object> claims, String subject) {
+        final var key = Keys.hmacShaKeyFor(JWT_SECRET.getBytes(StandardCharsets.UTF_8));
+
+        return Jwts.builder()
+                .setClaims(claims)
+                .setSubject(subject)
+                .setIssuedAt(new Date(System.currentTimeMillis()))
+                .setExpiration(new Date(System.currentTimeMillis() + JWT_TOKEN_VALIDITY * 1000))
+                .signWith(key)
+                .compact();
+    }
+
+    public  Boolean validateToken(String token, UserDetails userDetails) {
+        final var usernameFromUserDetails  = userDetails.getUsername();
+        final var usernameFromJWT  = this.getUsernameFromToken(token);
+
+        return (usernameFromUserDetails.equals(usernameFromJWT)) && !this.isTokenExpired(token);
     }
     
 }
